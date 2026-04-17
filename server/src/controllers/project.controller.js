dfae4f3d6709fcc234dc1342b4a13a95b6c9e6f8
Zuned khan <zunedkhan107@gmail.com>
2026-04-17 17:09:59 +0530

    get and create projects

---
+++ server/src/controllers/project.controller.js
@@ -0,0 +1,63 @@
+import Project from "../models/project.model.js";
+
+const getProject = async(req, res) => {
+    try {
+        const project = await Project.find().sort({ createdAt: -1 });
+
+        if(!projest){
+            res.status(401).json({
+                success: false,
+                message: "Project not Found!"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: project
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch projects"
+        })
+    }
+}
+
+const addProject = async(req, res) => {
+    try {
+        const { title, description, techStack, githubLink, liveLink } = req.body;
+    
+        if(!title){
+            res.status(404)
+               .jsson({
+                    success: false,
+                    message: "title is required!"
+               })
+        }
+    
+        const newProject = await Project.create({
+            title,
+            description,
+            techStack,
+            githubLink,
+            liveLink
+        })
+    
+        if(!newProject){
+            res.status(401).json({
+                seccess: false,
+                message: "Something went wrong in creating new project!"
+            })
+        }
+    
+        res.status(201).json({
+            success: true,
+            data: newProject
+        })
+    } catch (err) {
+        res.status(500).json({
+            seccess: false,
+            message: "Failed to create project"
+        })
+    }
+}
