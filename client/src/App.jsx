acfb6fe271ace82533dfdf035a7e6413492d2cb2
Zuned khan <zunedkhan107@gmail.com>
2026-04-18 19:11:47 +0530

    basic setup of frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-50, text-black'>
+      Hello in my smart portfolio
+    </div>
+  )
+}
+
+export default App
